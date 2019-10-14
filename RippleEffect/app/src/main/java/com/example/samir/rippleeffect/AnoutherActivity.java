7c8b8d629ecd63504dafcbb28b9b97b400926a5a
samirankumar11 <samirankumar11@gmail.com>
2019-10-14 16:31:27 +0600

    init progict

---
+++ RippleEffect/app/src/main/java/com/example/samir/rippleeffect/AnoutherActivity.java
@@ -0,0 +1,13 @@
+package com.example.samir.rippleeffect;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnoutherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anouther);
+    }
+}
---
+++ RippleEffect/app/src/main/java/com/example/samir/rippleeffect/OnTapListener.java
@@ -0,0 +1,10 @@
+package com.example.samir.rippleeffect;
+
+/**
+ * Created by samir on 3/5/2017.
+ */
+
+public interface OnTapListener
+{
+    public void onTapView(int position);
+}
